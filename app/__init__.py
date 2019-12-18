c3e2f4d571bceff1f7bea8aef090039ab040dfc3
zhaoyafei <yafei.zhao@shanbay.com>
2019-12-18 22:38:05 +0800

    add config module, pass secret_var by env

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
+import os
+
 from flask import Flask
+from werkzeug.utils import import_string
 
 from app.user.api import user
 
 
 app = Flask(__name__)
+
+env = os.environ.get('FLASK_ENV', 'development')
+config = import_string('configs.{}'.format(env))
+app.config.from_object(config)
 
 app.register_blueprint(user, url_prefix="/user")
---
+++ configs/development.py
@@ -0,0 +1,6 @@
+import os
+
+from configs.default import *  # noqa
+
+APP_SECRET = os.environ["APP_SECRET"]
+APP_ID = os.environ["APP_ID"]
