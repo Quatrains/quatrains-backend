9005c84c202cde3a8170bc017bd48b3775054f40
zhaoyafei <yafei.zhao@shanbay.com>
2020-03-21 18:04:26 +0800

    access recommend algorithm

--- app/Algs/recommend.py
+++ app/Algs/recommend.py
@@ -1,42 +1,77 @@
-from .utils.train import *
-from .model.v1_model import *
-from torch import autograd
-from .utils import readdata
-import torch.optim as optim
-import os
+# -*- coding:utf8 -*-
+'''
+Function:
+    Recommend poem for user.
+Parameters:
+    infomation_path: 'dict_SimilariyPopularityType.json' 的路径；
+    user_choose_type: list, ['悼亡', ’月‘, ...]，用户初入程序时选择的喜爱类型；
+    user_favorite_poem: list, 用户标记为喜爱的诗，idx；
+    user_recommended_poem: list, 最近30天给用户推荐过的诗，idx。
+代码运行说明：
+    需要下载 data/dict_SimilariyPopularityType.json；
+    需要本代码，以及 utils/utils.py.
+'''
+import random
+import json
 
-VOCAB_SIZE = len(readdata.word_to_ix)
-NUM_LABELS = 2  # ENGLISH  SPANISH
 
+class Predict:
+    def __init__(self, infomation_path):
+        self.infomation_path = infomation_path
+        self.loadData()
 
-class predict():
-    def __init__(self, model_save_path):
-        self.model = BoWClassifier(NUM_LABELS, VOCAB_SIZE)
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.1)
-        self.model_save_path = model_save_path
+    def loadData(self):
+        with open(self.infomation_path, encoding='utf8') as f:
+            info = json.load(f)
+        self.idx_likescount_list = info['popularity']
+        self.type_idx_dict = info['type']
+        self.idx_mostsimiliar_dict = {}
+        for _idx, _similars in info['similarity'].items():
+            self.idx_mostsimiliar_dict[int(_idx)] = _similars
 
-    def train(self):
-        Train(self.model, self.optimizer, self.model_save_path)
+    def _randomSelectPoem(self, user_choose_type, user_favorite_poem, user_recommended_poem):
+        # self.idx_likescount_list - self.user_favorite_poem - self.recommend_poem
+        to_be_choosed = []
+        if user_choose_type:
+            _type = random.sample(user_choose_type, len(user_choose_type)//2) 
+            for _t in _type:
+                to_be_choosed += self.type_idx_dict[_t]
+            to_be_choosed = list(set(to_be_choosed).difference(set(user_favorite_poem)).difference(user_recommended_poem))
+            if to_be_choosed:
+                return random.choice(to_be_choosed)
+        to_be_choosed = list(set(self.idx_likescount_list).difference(set(user_favorite_poem)).difference(user_recommended_poem))
+        return random.choice(to_be_choosed)
 
-    def predict(self, data):
-        print('==================')
-        if os.path.exists(self.model_save_path):
-            checkpoint = torch.load(model_save_path)
-            self.model.load_state_dict(checkpoint['model_state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            self.model.eval()
-            print('saved model is used')
+    def predict(self, user_choose_type, user_favorite_poem, user_recommended_poem):
+        # 第一次进入系统
+        if not user_favorite_poem and not user_recommended_poem: 
+            # 未选择喜爱类型
+            if not user_choose_type: 
+                return random.choice(self.idx_likescount_list)
+            # 选择喜爱类型
+            else:
+                _type = random.choice(user_choose_type)
+                return random.choice(self.type_idx_dict[_type])
+
+        # 非第一次进入系统
         else:
-            self.train()
+            # 用户有标记喜爱的诗词
+            if user_favorite_poem:
+                idx = random.choice(user_favorite_poem)
+                to_be_choosed = list(set(self.idx_mostsimiliar_dict[idx]).difference(set(user_recommended_poem)))
+                if to_be_choosed:
+                    return random.choice(to_be_choosed)
+                else:
+                    return self._randomSelectPoem(user_choose_type, user_favorite_poem, user_recommended_poem)
+            # 用户未标记喜爱的诗词
+            else:
+                return self._randomSelectPoem(user_choose_type, user_favorite_poem, user_recommended_poem)
 
-        for instance, label in data:
-            bow_vec = autograd.Variable(
-                make_bow_vector(instance, readdata.word_to_ix))
-            log_probs = self.model(bow_vec)
-            print(log_probs)
-        return log_probs
+
+# infomation_path = 'dict_SimilariyPopularityType.json'
+# s = Predict(infomation_path)
+# idx = s.predict(["悼亡", "月"], [], [])
+# print(idx)
 
 
-model_save_path = 'app/Algs/pre_trained/v1_model.pkl'
-s = predict(model_save_path)
-s.predict(readdata.test_data)
+
--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,11 @@
 from flasgger import Swagger
 from peeweext.flask import Peeweext
 
+from app.Algs.recommend import Predict
+
 
 swagger = Swagger(template_file="docs/openapi_template.yml")
 
 pwdb = Peeweext()
+
+predict = Predict("app/Algs/dict_SimilariyPopularityType.json")
